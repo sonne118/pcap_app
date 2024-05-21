3ce236d5f936eccad70967a90adc2b577f5e0255
sonne <_sonne_@ukr.net>
2024-05-21 23:18:04 +0300

    .

--- get_packages/main.cpp
+++ get_packages/main.cpp
@@ -146,12 +146,8 @@ void packetHandler(u_char* userData, const struct pcap_pkthdr* pkthdr, const u_c
     char* ethernet_shost = nullptr;
     char* ethernet_dhost = nullptr;
     std::string check = "";
-    const unsigned char* ptr;
     const unsigned char* ptr1;
     const unsigned char* ptr2;
-    int i;
-
-
     u_int sourcePort, destPort;
     u_char* data;
     int dataLength = 0;
@@ -177,9 +173,7 @@ void packetHandler(u_char* userData, const struct pcap_pkthdr* pkthdr, const u_c
             destPort = ntohs(tcpHeader->dport);
             data = (u_char*)(packet + sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct tcphdr));
             dataLength = pkthdr->len - (sizeof(struct ether_header) + sizeof(struct ip) + sizeof(struct tcphdr));
-
-            // convert non-printable characters, other than carriage return, line feed,
-            // or tab into periods when displayed.
+            
             for (int i = 0; i < dataLength; i++) {
                 if ((data[i] >= 32 && data[i] <= 126) || data[i] == 10 || data[i] == 11 || data[i] == 13) {
                     dataStr += (char)data[i];
