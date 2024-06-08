c6946d6cb2abd1cd6ded3871345fc930c73ef65b
sonne <_sonne_@ukr.net>
2024-06-08 20:55:08 +0300

    .

---
+++ WpfApp/Services/Worker/Worker.cs
@@ -0,0 +1,65 @@
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading;
+using System.Threading.Tasks;
+using WpfApp.Model;
+
+namespace WpfApp.Services.Worker
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            BinaryReader value = default; Snapshot res = default;
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+                await using var pipe = new NamedPipeClientStream(".", "testpipe", PipeDirection.InOut);
+                using (BinaryReader stream = new BinaryReader(pipe))
+                {
+                    pipe.Connect(100000);
+                    pipe.ReadMode = PipeTransmissionMode.Byte;
+
+                    Func<BinaryReader, Task<Snapshot>> func = (value) => Task.Run(() => ReadClass.ReadMessage(value));
+
+                    var pool = new ObjectPool<BinaryReader, Task<Snapshot>>(() =>
+                    {
+                        return func;
+                    });
+
+                    pool.Return(func);// first Task in poll
+                    pool.Return(func);// second Task in poll 
+                    pool.Return(func); //third Task in poll for any case ... improving scalability
+                    while (pipe.IsConnected)
+                    {
+                        var result = pool.Get();
+                        try
+                        {
+                            res = await result(stream);
+                            Console.WriteLine(res);
+                        }
+                        finally
+                        {
+                            pool.Return(result);
+                        }
+                    }
+                }
+
+                pipe.Close();
+                await Task.Delay(10000, stoppingToken);
+            }
+        }
+    }
+}
---
+++ WpfApp/Services/Worker/WorkerProcess.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfApp.Services.Worker
+{
+    public class WorkerProcess : BackgroundService
+    {
+        [DllImport("sniffer_packages.dll")]
+        extern static void fnCPPDLL();
+        readonly private int timeout;
+        //readonly private string path;
+        public WorkerProcess()
+        {
+            //    path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "sniffer_packages.exe"));
+            timeout = 10000;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+
+
+            ThreadPool.QueueUserWorkItem(delegate
+            {
+                fnCPPDLL();
+                //    var proc = new Process();
+                //    try
+                //    {
+                //        proc.StartInfo.FileName = path;
+                //        proc.StartInfo.UseShellExecute = false;
+                //        proc.StartInfo.RedirectStandardInput = true;
+                //        proc.Start();
+                //    }
+                //    finally
+                //    {
+                //        proc.WaitForExit();
+                //        proc.Close();
+                //    }
+            });
+            await Task.Delay(timeout, stoppingToken);
+        }
+    }
+
+}
