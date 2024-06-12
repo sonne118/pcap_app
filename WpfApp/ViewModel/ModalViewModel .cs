258dc9c4fdc8346b205c1f0360286e4424ac857e
sonne <_sonne_@ukr.net>
2024-06-12 17:16:37 +0300

    .

---
+++ WpfApp/ViewModel/ModalViewModel .cs
@@ -0,0 +1,39 @@
+﻿using CoreModel.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM
+{
+    
+    public class ModalViewModel : INotifyPropertyChanged
+    {
+        private SnifferData _data;
+        public SnifferData Data1
+        {
+            get { return _data; }
+            set
+            {
+                if (_data != value)
+                {
+                    _data = value;
+                    OnPropertyChanged(nameof(Data1));
+                }
+            }
+        }
+
+        public ModalViewModel(SnifferData data)
+        {
+            Data1 = data;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
