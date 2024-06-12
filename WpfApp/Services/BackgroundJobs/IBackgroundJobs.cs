97f0878a1c686f60825d901dc7fd05fb73184b3f
sonne <_sonne_@ukr.net>
2024-06-12 12:47:46 +0300

    .

---
+++ WpfApp/Services/BackgroundJobs/IBackgroundJobs.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Concurrent;
+
+namespace WpfApp.Services.BackgroundJob
+{
+    public interface IBackgroundJobs<T>
+    {
+        ConcurrentQueue<T> BackgroundTasks { get; set; }
+    }
+}
