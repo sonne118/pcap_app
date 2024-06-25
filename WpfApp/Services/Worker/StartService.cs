6eefb42499c0f9ccdaa00c7a5b3ba9d9f0013411
sonne118 <_sonne_@ukr.net>
2024-06-25 21:03:16 +0300

    add StartService

---
+++ WpfApp/Services/Worker/StartService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfApp.Services.Worker
+{
+    public class StartService : IHostedService
+    {
+        static EventWaitHandle _eventWaitHandle;
+        static StartService()
+        {
+            _eventWaitHandle = new EventWaitHandle(false, EventResetMode.ManualReset, @"Global\sniffer");
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _eventWaitHandle.Reset();
+            return Task.CompletedTask;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _eventWaitHandle.Set();
+            return Task.CompletedTask;
+        }
+
+    }
+}
