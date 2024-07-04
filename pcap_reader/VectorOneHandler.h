43f77659b25d729ca045d4ce509cbd58016389c3
sonne <_sonne_@ukr.net>
2024-07-05 00:21:30 +0300

    .

---
+++ pcap_reader/VectorOneHandler.h
@@ -0,0 +1,106 @@
+#ifndef VECTORONEHANDLER_H
+#define VECTORONEHANDLER_H
+#include <IPv4Layer.h>
+#include <TcpLayer.h>
+#include <Packet.h>
+#include <PcapFileDevice.h>
+#include "EthLayer.h"
+#include "HttpLayer.h"
+#include "stdlib.h"
+#include <iostream>
+#include "SystemUtils.h"
+#include "PcapLiveDeviceList.h"
+#include <cstdlib>
+#include <Windows.h>
+#include <array>
+#include <stdio.h>
+#include <strsafe.h>
+#include <combaseapi.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <charconv>
+#include <cstddef>
+#include <atlsafe.h>
+#include <combaseapi.h>
+#include<struct.h>
+#include<Handler.h>
+#include<AbstractHandler.h>
+
+using namespace std;
+std::string getAgentAsString(pcpp::HttpRequestLayer* httpRequestLayer);
+
+class VectorOneHandler : public AbstractHandler
+{
+	std::vector<std::unique_ptr<vStruct>> request;
+public:
+	~VectorOneHandler() {};
+
+public:
+	void Handle(std::vector<std::unique_ptr<vStruct>>& request)
+	{
+		// open a pcap file for reading
+		std::string  path = "D:/repo/test2/pcap_app/pcap_app/pcap_reader/message2.pcap";
+
+		pcpp::PcapFileReaderDevice reader(path.c_str());
+		if (!reader.open())
+		{
+			printf("Error opening the pcap file\n");
+			return;
+		}
+
+		// read the first (and only) packet from the file
+		pcpp::RawPacket rawPacket;
+		if (!reader.getNextPacket(rawPacket))
+		{
+			printf("Couldn't read the first packet in the file\n");
+			return;
+		}
+		int i = 1;
+		std::string user_agent;
+		pcpp::Packet parsedPacket(&rawPacket);
+		pcpp::EthLayer* ethernetLayer = parsedPacket.getLayerOfType<pcpp::EthLayer>();
+		pcpp::HttpRequestLayer* httpRequestLayer = parsedPacket.getLayerOfType<pcpp::HttpRequestLayer>();
+		pcpp::Layer* curLayer = parsedPacket.getFirstLayer();
+		while (reader.getNextPacket(rawPacket))
+		{
+			i++;
+			// parse the raw packet into a parsed packet
+			pcpp::Packet parsedPacket(&rawPacket);
+			ethernetLayer = parsedPacket.getLayerOfType<pcpp::EthLayer>();
+			httpRequestLayer = parsedPacket.getLayerOfType<pcpp::HttpRequestLayer>();
+			curLayer = parsedPacket.getFirstLayer();
+
+			// verify the packet is IPv4
+			if (httpRequestLayer == NULL)
+			{
+				user_agent = "none";
+			}
+			else
+			{
+				std::string user_agent = httpRequestLayer->getFieldByName(PCPP_HTTP_USER_AGENT_FIELD)->getFieldValue();
+				user_agent = getAgentAsString(httpRequestLayer);
+			}
+
+			if (parsedPacket.isPacketOfType(pcpp::IPv4))
+			{
+				OneStruct* st = new OneStruct(i,
+					parsedPacket.getLayerOfType<pcpp::IPv4Layer>()->getSrcIpAddress().toString(),
+					parsedPacket.getLayerOfType<pcpp::IPv4Layer>()->getDstIpAddress().toString(),
+					ethernetLayer->getSourceMac().toString(),
+					ethernetLayer->getDestMac().toString(),
+					user_agent);
+				request.emplace_back(std::make_unique<OneStruct>(*st));
+			}
+		}
+		reader.close();
+
+		if (getHandle() != nullptr)
+		{
+			AbstractHandler::Handle(request);
+		}
+	}
+};
+
+#endif
+
---
+++ pcap_reader/main.cpp
@@ -0,0 +1,55 @@
+#include<atlsafe.h>
+#include<vector>
+#include<struct.h>
+#include<Handler.h>
+#include<AbstractHandler.h>
+#include<VectorOneHandler.h>
+#include<VectorTwoHandler.h>
+
+static std::vector<SecondStruct> ClientCode(int* size)
+	{
+	    std::vector<SecondStruct> vec{};
+		std::vector<std::unique_ptr<vStruct>> request{};
+		vStruct* tmpBase = nullptr;
+		SecondStruct* tmp;
+
+		VectorOneHandler* vectorOne = new VectorOneHandler();
+		VectorTwoHandler* vectorTwo = new VectorTwoHandler();
+
+		vectorOne->SetNext(vectorTwo);
+		vectorOne->Handle(request);
+
+		for (auto& v : request) {
+			tmpBase = v.get();
+			tmp = static_cast<SecondStruct*>(tmpBase);
+			SecondStruct st{};
+			st.id = tmp->id;
+			st.source_ip = tmp->source_ip;
+			st.dest_ip = tmp->dest_ip;
+			st.mac_source = tmp->mac_source;
+			st.mac_destin = tmp->mac_destin;
+			st.user_agent = tmp->user_agent;
+			vec.push_back(st);
+			v.reset();
+		}
+		*size = request.size();
+
+		vectorOne->~VectorOneHandler();
+		vectorTwo->~VectorTwoHandler();
+
+		return vec;
+	}
+
+std::vector<SecondStruct> result{};
+extern "C" __declspec(dllexport) SecondStruct * Predict(int* size1, const char* path2D)
+{
+	int size;
+	result = ClientCode(&size);
+	*size1 = size;
+
+	return result.data();
+}
+
+
+
+
