d1906bd7be940abfe839b4fb009f51233baec8c9
sonne <_sonne_@ukr.net>
2024-07-05 16:50:31 +0300

    comment debug code cpp dll

--- WpfApp/IPC/Ptr/GetStreamPtr.cs
+++ WpfApp/IPC/Ptr/GetStreamPtr.cs
@@ -5,9 +5,9 @@ namespace wpfapp.IPC.Ptr
 {
     public class GetStreamPtr
     {
-        //[DllImport("sniffer_packages.dll")]
-        [DllImport(@"D:\repo\test2\pcap_app12\2\pcap_app\x64\Debug\sniffer_packages.dll", EntryPoint =
-        "fnCPPDLL", CallingConvention = CallingConvention.StdCall)]
+        [DllImport("sniffer_packages.dll")]
+        //[DllImport(@"D:\repo\test2\pcap_app12\2\pcap_app\x64\Debug\sniffer_packages.dll", EntryPoint =
+        //"fnCPPDLL", CallingConvention = CallingConvention.StdCall)]
         extern static void fnCPPDLL(int dev);
 
         private static Thread _workerThread;
