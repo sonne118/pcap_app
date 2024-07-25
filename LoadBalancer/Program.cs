f65788546f067ca33a9617517ca679912f4a6e23
sonne <_sonne_@ukr.net>
2024-07-25 19:17:40 +0300

    adding Dockerfile on Server and LoadBalancer

--- LoadBalancer/Program.cs
+++ LoadBalancer/Program.cs
@@ -1,7 +1,21 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
 var builder = WebApplication.CreateBuilder(args);
+
+string? certPath = Environment.GetEnvironmentVariable("CERTIFICATE_PATH");
+string? certPass = Environment.GetEnvironmentVariable("CERTIFICATE_PASSWORD");
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.WebHost.ConfigureKestrel(option =>
+{
+    option.ListenAnyIP(5001, listenOptions =>
+    {
+        listenOptions.UseHttps(certPath, certPass);
+        listenOptions.Protocols = HttpProtocols.Http2;
+    });
+});
 
 var app = builder.Build();
 
