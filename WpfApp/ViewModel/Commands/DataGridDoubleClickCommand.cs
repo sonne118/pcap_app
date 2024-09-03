fbebc2a4d31060cb874f01fc48fb563775abba0d
sonne <_sonne_@ukr.net>
2024-09-03 15:16:08 +0300

    add DataGridDoubleClickCommand class to handle double click

---
+++ WpfApp/ViewModel/Commands/DataGridDoubleClickCommand.cs
@@ -0,0 +1,41 @@
+﻿using CoreModel.Model;
+using GalaSoft.MvvmLight.CommandWpf;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MVVM
+{
+    public class DataGridDoubleClickCommand
+    {
+        public RelayCommand<(RoutedEventArgs, object)> _showCommand { get; private set; }
+        private MainWindow _mainWindow;
+        public DataGridDoubleClickCommand(MainWindow mainWindow)
+        {
+            _showCommand = new RelayCommand<(RoutedEventArgs, object)>(OnExecuted);
+            _mainWindow = mainWindow;
+        }
+        public ICommand ShowCommand
+        {
+            get
+            {
+                if (_showCommand == null)
+                    _showCommand = new RelayCommand<(RoutedEventArgs, object)>(OnExecuted);
+                return _showCommand;
+            }
+        }
+
+        private void OnExecuted((RoutedEventArgs events, object commandParameter) args)
+        {
+            var e = args.events;
+            var data = args.commandParameter as StreamingData;
+            ModalViewModel viewModel = new ModalViewModel(data);
+            ModalWindow modalWindow = new ModalWindow
+            {
+                DataContext = viewModel.ModalData
+            };
+
+            modalWindow.ShowDialog();
+        }
+    }
+}
+
