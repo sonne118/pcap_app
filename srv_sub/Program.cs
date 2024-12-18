2806d6895f2cdadd3cb16c99134778d848287987
sonne118 <_sonne_@ukr.net>
2024-12-18 16:29:41 +0200

    add srv_sub service

---
+++ srv_sub/Program.cs
@@ -0,0 +1,14 @@
+using kafka;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: true);
+
+
+builder.Services.AddKafkaSubscribe("");
+
+var app = builder.Build();
+
+
+
+app.Run();
+
