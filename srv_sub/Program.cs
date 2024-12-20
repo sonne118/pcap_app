94ba8b4d021e06a8550bad11769f88b56c5b3d15
sonne118 <_sonne_@ukr.net>
2024-12-20 21:22:36 +0200

    added docker compose Consul

--- srv_sub/Program.cs
+++ srv_sub/Program.cs
@@ -1,14 +1,12 @@
 using kafka;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("appsettings.json", optional: true);
 
-
 builder.Services.AddKafkaSubscribe("");
 
 var app = builder.Build();
-
-
 
 app.Run();
 
