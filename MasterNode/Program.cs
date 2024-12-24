a5ab16cba00d1982494c55d7d9fb6d73f1a091f7
sonne118 <_sonne_@ukr.net>
2024-12-24 21:26:18 +0200

    add two MasterNode projects and MongoDbReplicaSet project

---
+++ MasterNode/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+public class MasterNodeService
+{
+    public static async Task MasterNodeStartAsync()
+    {
+        var listener = new TcpListener(IPAddress.Any, 6142);
+        listener.Start();
+        Console.WriteLine("Master Node is running...");
+
+        while (true)
+        {
+            var client = await listener.AcceptTcpClientAsync();
+            _ = HandleClientAsync(client);
+        }
+    }
+
+    private static async Task HandleClientAsync(TcpClient client)
+    {
+      
+    }
+
+  
+    public static async Task  Main(string[] args)
+    { 
+       await MasterNodeStartAsync();
+    }
+}
+
