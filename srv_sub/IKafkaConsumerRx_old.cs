e4396eccf3f0919b0f4a7309725ba468dc316ee5
sonne118 <_sonne_@ukr.net>
2025-01-08 21:59:32 +0200

    add new low-level  and higher-level of Rx Consumer Kafka

---
+++ srv_sub/IKafkaConsumerRx_old.cs
@@ -0,0 +1,7 @@
+﻿namespace srv_sub
+{
+    public interface IKafkaConsumerRx_old
+    {
+       public Task StartConsume(CancellationToken stoppingToken);
+    }
+}
---
+++ srv_sub/KafkaConsumerEx.cs
@@ -0,0 +1,28 @@
+﻿
+namespace srv_sub
+{
+    public class KafkaConsumerEx : BackgroundService
+    {
+        private readonly IKafkaConsumerRx_old? _kafkaConsumerRx;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<KafkaConsumerRx_old> _logger;
+        private readonly string _topic;
+        public KafkaConsumerEx(IKafkaConsumerRx_old kafkaConsumerRx,
+                               IConfiguration configuration,
+                               ILogger<KafkaConsumerRx_old> logger,
+                               string topic
+            )
+        {
+            _kafkaConsumerRx = kafkaConsumerRx;
+            _configuration = configuration;
+            _logger = logger;
+            _topic = topic;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _kafkaConsumerRx.StartConsume(stoppingToken);
+            await Task.Delay(1000);
+        }
+    }
+}
